78706dd258a4e759f41b0ae9051e154752e604da
a852220 <a852220@gmail.com>
2025-07-13 06:07:16 +0000

    11

--- lesson5/lesson5_1.sql
+++ lesson5/lesson5_1.sql
@@ -1,5 +1,28 @@
-ˋˋˋSQL
-SELECT SUM(總確診數) AS 總確診數
-FROM world
-WHERE 日期 BETWEEN '2020-01-01' AND '2020-12-31';
-ˋˋˋ
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS  branch;
+
+CREATE TABLE employee(
+	emp_id SERIAL,
+	name VARCHAR(20),
+	birth_date DATE,
+	sex VARCHAR(1),
+	salary INT,
+	branch_id INT,
+	sup_id INT,
+ 	PRIMARY KEY(emp_id)
+);
+
+CREATE TABLE branch(
+	branch_id INT,
+	branch_name VARCHAR(20),
+	manager_id INT,
+	PRIMARY KEY(branch_id),
+	FOREIGN KEY(manager_id)
+	REFERENCES employee(emp_id) ON DELETE SET NULL
+);
+
+ALTER TABLE employee ADD FOREIGN KEY(branch_id)
+REFERENCES branch(branch_id) ON DELETE SET NULL;
+
+ALTER TABLE employee ADD FOREIGN KEY(sup_id)
+REFERENCES employee(emp_id) ON DELETE SET NULL;
