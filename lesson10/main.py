c8f777aae86d65ee73f8712d1ad04a1cdfdd7a2d
tom <a852220@gmail.com>
2025-08-17 00:55:44 +0000

    更新 .gitignore，新增 __pycache__ 目錄；更新 main.py，整合 Streamlit 顯示車站資料；更新 requirements.txt，確保包含 Streamlit；新增 lession7_1.ipynb 檔案

--- lesson10/main.py
+++ lesson10/main.py
@@ -1,12 +1,13 @@
 import source
+import streamlit as st
 
 def main():
+    st.title("台鐵車站名稱列表")
     results = source.get_stations_names()
     if results:
-        for station in results:
-            print(station)
+        st.dataframe(results, width=400, height=600)
     else:
-        print("無法取得車站資料")
+        st.error("無法取得車站資料")
 
 if __name__ == "__main__":
     main()
